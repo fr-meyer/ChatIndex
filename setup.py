89224ee4df6814e3c8e0947280d7f9e1eb8da52e
Franck MEYER <franck.meyer@kaist.ac.kr>
2026-06-12 18:43:10 +0900

    release: sync v0.1.1 promotion file setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def read_requirements(filename):
 
 setup(
     name="ictree",
-    version="0.1.0",
+    version="0.1.1",
     description="Incremental Context Tree for hierarchical conversation management",
     long_description=read_file("README.md"),
     long_description_content_type="text/markdown",
